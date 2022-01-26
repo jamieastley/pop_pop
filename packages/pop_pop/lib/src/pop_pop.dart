6b2f2efed6847865e2300085fa9064685afd2dac
jamieastley <6836556+jamieastley@users.noreply.github.com>
2022-01-27 01:13:25 +1100

    fix debug logging naming inconsistency

--- packages/pop_pop/lib/src/pop_pop.dart
+++ packages/pop_pop/lib/src/pop_pop.dart
@@ -31,8 +31,8 @@ abstract class PopPop<S extends PopPopState, T extends Object> {
   /// Optional timer interface for timed game modes.
   final PopPopTimer<T>? timer;
 
-  /// Enable library debug logging.
-  final bool isLoggingEnabled;
+  /// Enable debug logging.
+  final bool enableLogging;
 
   /// Creates an implementation of a [PopPop].
   ///
@@ -42,7 +42,7 @@ abstract class PopPop<S extends PopPopState, T extends Object> {
     required this.bubbleTheme,
     this.audioPlayer,
     this.timer,
-    this.isLoggingEnabled = false,
+    this.enableLogging = false,
   });
 
   /// Cache the generated grid size/count for the device.
